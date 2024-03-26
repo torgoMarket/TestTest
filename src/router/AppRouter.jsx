b573832abbec9676e0a14bec8e8136d9eb589b7d
Amir256456 <amir1008047@gmail.com>
2024-03-26 08:08:37 +0500

    test

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { About } from '../components/About'
+import { Home } from '../components/Home'
+export const AppRouter = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/home' element={<Home />}></Route>
+				<Route path='/about' element={<About />}></Route>
+			</Routes>
+		</BrowserRouter>
+	)
+}
